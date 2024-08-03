150ba5c238f11f35ee70a74a704ba024c438012a
benja <benjammunoz@alumnos.uai.cl>
2024-08-02 22:11:33 -0400

    Se agrego componente para h1, componente para logos de aliados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
-import { NavbarDemo } from "@/app/navbar";
+import { NavbarDemo } from "@/app/Navbar";
+import { MacbookScrollDemo } from "@/app/MacbookScrollDemo";
+import { InfiniteMovingCardsDemo } from "@/app/InfiniteMovingCards";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold">dotu-landing-next</h1>
-      <NavbarDemo></NavbarDemo>
+      <NavbarDemo/>
+      <MacbookScrollDemo/>
+      <InfiniteMovingCardsDemo/>
     </main>
   );
 }
