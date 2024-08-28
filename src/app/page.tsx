5557606a807cb977d5e70f9881ab7ddcddef6288
benja <benjammunoz@alumnos.uai.cl>
2024-08-28 13:11:14 -0400

    se agrega el componente de infinite card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,14 @@ import { HeroScrollDemo} from "@/app/tablet";
 import { Testimonios } from "../components/ui/Testimonios";
 import "./globals.css";
 import  Component  from "../components/ui/ScrolljackingLayout";
+import {InfiniteMovingCardsDemo} from "../app/infinitemovingcard";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between  pb-0">
       <Navbar/>
       <HeroScrollDemo/>
+      <InfiniteMovingCardsDemo/>
       <Component></Component>
       <Testimonios/>
       <Form/>
