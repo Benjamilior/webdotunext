09856c374e5c588b34ef30f49e0845db34be3404
benja <benjammunoz@alumnos.uai.cl>
2024-09-11 22:13:29 -0300

    Configuraciones varias

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('next').NextConfig} */
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Definir __dirname en ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
     images: {
-        domains: ['picsum.photos','www.dropbox.com','drive.google.com'],
-    }
+        domains: ['picsum.photos', 'www.dropbox.com', 'drive.google.com'],
+    },
+    webpack(config) {
+        config.resolve.alias['@'] = path.join(__dirname, 'src');
+        return config;
+    },
 };
 
 export default nextConfig;
+
